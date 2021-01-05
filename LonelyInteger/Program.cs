9066842425f921f0e9b3dc7c718f16938bec9ff3
Monna9505 <76119406+Monna9505@users.noreply.github.com>
2021-01-06 00:03:33 +0200

    Add files via upload

---
+++ LonelyInteger/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LonelyInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> arr = Console.ReadLine().Split().Select(int.Parse).OrderBy(p => p).ToList();
+            bool even = true;
+            int current = 0;
+
+            while (true)
+            {
+                current = arr[0];
+
+                if (arr.Count == 1)
+                {
+                    Console.WriteLine(current);
+                    return;
+                }
+                else if (current == arr[1])
+                {
+                    even = true;
+                    arr.RemoveAll(p => p == current);
+                }
+                else
+                {
+                    Console.WriteLine(current);
+                    return;
+                }
+            }
+        }
+    }
+}
+
