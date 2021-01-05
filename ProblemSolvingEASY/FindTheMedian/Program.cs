5e9ff7caccc5de423f67b5065834ad1bdcb36865
Monna9505 <76119406+Monna9505@users.noreply.github.com>
2021-01-06 00:05:21 +0200

    Add files via upload

---
+++ ProblemSolvingEASY/FindTheMedian/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace FindTheMedian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).OrderBy(n => n).ToArray();
+            int median = nums[nums.Length / 2];
+
+            Console.WriteLine(median);
+        }
+    }
+}
